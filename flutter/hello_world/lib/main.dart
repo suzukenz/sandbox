a58fb1f1a68bfcd79cff1fc921d602f1f44ec3b4
kenji suzuki <xkenji.suzuki@gmail.com>
2020-11-01 22:24:28 +0900

    Adding interactivity to your Flutter app まで

--- flutter/hello_world/lib/main.dart
+++ flutter/hello_world/lib/main.dart
@@ -35,11 +35,7 @@ class MyApp extends StatelessWidget {
                 ],
               ),
             ),
-            Icon(
-              Icons.star,
-              color: Colors.red[500],
-            ),
-            Text('41'),
+            FavoriteWidget(),
           ],
         ));
     Color color = Theme.of(context).primaryColor;
@@ -109,5 +105,51 @@ class MyApp extends StatelessWidget {
         )
       ],
     );
+  }
+}
+
+class FavoriteWidget extends StatefulWidget {
+  @override
+  _FavoriteWidgetState createState() => _FavoriteWidgetState();
+}
+
+class _FavoriteWidgetState extends State<FavoriteWidget> {
+  bool _isFavorited = true;
+  int _favoriteCount = 41;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Container(
+            padding: EdgeInsets.all(0),
+            child: IconButton(
+              padding: EdgeInsets.all(0),
+              alignment: Alignment.centerRight,
+              icon: (_isFavorited ? Icon(Icons.star) : Icon(Icons.star_border)),
+              color: Colors.red[500],
+              onPressed: _toggleFavorite,
+            )),
+        SizedBox(
+          width: 18,
+          child: Container(
+            child: Text('$_favoriteCount'),
+          ),
+        ),
+      ],
+    );
+  }
+
+  void _toggleFavorite() {
+    setState(() {
+      if (_isFavorited) {
+        _favoriteCount -= 1;
+        _isFavorited = false;
+      } else {
+        _favoriteCount += 1;
+        _isFavorited = true;
+      }
+    });
   }
 }
